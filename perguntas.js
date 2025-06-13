52a5e7ad29165a108e213a171fc3ea7bc1281e68
Cimili-cimili <texeira.camila@escola.pr.gov.br>
2025-06-13 08:49:00 -0300

    aula 6 n2

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Português',
+    '0 que é um resumo literário?',
+    'É um resumo que não contém opinião e apresenta as informações importantes do texto.'
+)
+
+criaCartao(
+    'HISTÓRIA',
+    'Onde viviam os escravos no engenho?',
+    'Os escravos viviam na Senzala.'
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'O que é El Niño?',
+    'É um fenômeno climático que promove o aquecimento anormal das águas do oceano Pacífico equatorial.'
+)
+
+criaCartao(
+    'BIOLOGIA',
+    'Qual é a estrutura de uma bactéria?',
+    'A bactéria é formada, principalmente, por parede celular e material genético..'
+)
