bf5933ba1ab697eb0fd97a456b35f168388b8846
Cimili-cimili <texeira.camila@escola.pr.gov.br>
2025-07-04 12:07:00 -0300

    444

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,5 @@
 criaCartao(
-    'Português',
+    'PORTUGUÊS',
     '0 que é um resumo literário?',
     'É um resumo que não contém opinião e apresenta as informações importantes do texto.'
 )
@@ -19,5 +19,41 @@ criaCartao(
 criaCartao(
     'BIOLOGIA',
     'Qual é a estrutura de uma bactéria?',
-    'A bactéria é formada, principalmente, por parede celular e material genético..'
+    'A bactéria é formada, principalmente, por parede celular e material genético.'
 )
+
+criaCartao(
+    'QUÍMICA',
+    'Quantas ligações o carbono faz?',
+    'O carbono faz 4 ligações.'
+)
+
+
+criaCartao(
+    'FÍSICA',
+    'Qual é a velocidade da luz?',
+    'A velocidade da luz é de 300.000 km/s.'
+)
+
+
+criaCartao(
+    'MATEMÁTICA',
+    'Quanto é 7x8?',
+    'O resultado é 56.'
+)
+
+
+criaCartao(
+    'INGLÊS',
+    'Como dizer cachorro em inglês?',
+    'Cachorro em inglês é dog.'
+)
+
+
+criaCartao(
+    'ED. FINANCEIRA',
+    'Quais são os três poderes?',
+    'Executivo, Legislativo e Judiciário.'
+)
+
+
